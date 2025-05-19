d435819e74703532d0cc006976a0f27bc66443e8
Bumgu Kang <flrhyme9@gmail.com>
2025-05-19 15:35:07 +0900

    refactor: seperate root, page handler from network file

--- handlers/root.go
+++ handlers/root.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"path/filepath"
+	"spotify-go/internal/templates"
 	"spotify-go/service"
 	"sync"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
@@ -24,4 +28,21 @@ func NewHandler(s *service.Service) *Handler {
 	})
 
 	return handlerInstance
+}
+
+func (h *Handler) CssHandler(c *gin.Context) {
+	path := filepath.Clean(c.Param("filepath"))
+
+	if filepath.Ext(path) == ".css" {
+		c.Header("Content-type", "text/css; charset=utf-8")
+	}
+	c.File("./static" + path)
+}
+
+func (h *Handler) HomeHandler(c *gin.Context) {
+	templates.Home().Render(c.Request.Context(), c.Writer)
+}
+
+func (h *Handler) SpotifyPageHandler(c *gin.Context) {
+	templates.SpotifyPage().Render(c.Request.Context(), c.Writer)
 }
--- networks/root.go
+++ networks/root.go
@@ -1,9 +1,7 @@
 package networks
 
 import (
-	"path/filepath"
 	"spotify-go/handlers"
-	"spotify-go/internal/templates"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,23 +15,12 @@ func NewNetwork(h *handlers.Handler) *Network {
 		engine: gin.New(),
 	}
 
+	n.registerGET("/static/*filepath", h.CssHandler)
+	n.registerGET("/home", h.HomeHandler)
+	n.registerGET("/spotify", h.SpotifyPageHandler)
+
 	NewSpotifyRouter(n, h.Spotify)
 
-	n.registerGET("/static/*filepath", func(c *gin.Context) {
-		path := filepath.Clean(c.Param("filepath"))
-
-		if filepath.Ext(path) == ".css" {
-			c.Header("Content-type", "text/css; charset=utf-8")
-		}
-		c.File("./static" + path)
-	})
-
-	n.registerGET("/home", func(c *gin.Context) {
-		templates.Home().Render(c.Request.Context(), c.Writer)
-	})
-	n.registerGET("/spotify", func(c *gin.Context) {
-		templates.SpotifyPage().Render(c.Request.Context(), c.Writer)
-	})
 	return n
 }
 
